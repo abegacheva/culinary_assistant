224c37537ed41c5998a09e8921139b0d03fdc3ff
Starrnastish <144003753+abegacheva@users.noreply.github.com>
2026-06-12 11:49:30 +0300

    Create Nutrition.cs

---
+++ Models/Nutrition.cs
@@ -0,0 +1,13 @@
+namespace Culinary_Assistant.Models
+{
+    public class Nutrition
+    {
+        public double Calories { get; set; }
+
+        public double Protein { get; set; }
+
+        public double TotalCarbohydrates { get; set; }
+
+        public double TotalFat { get; set; }
+    }
+}
